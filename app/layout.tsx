0ace29885145c4832dc436abcbc2555a9b6a54ec
Oudart Matéo <mateooudart@gmail.com>
2024-12-15 16:38:33 +0100

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           <Navbar />
           {children}
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
